90cd23568c836fe1d09998578e94bfe8118285a3
Tobias Zucali <tobias@zucali.at>
2022-11-21 23:27:05 +0100

    App: add percentage input

--- src/routes/App/index.tsx
+++ src/routes/App/index.tsx
@@ -1,10 +1,27 @@
+import { useState } from 'react';
+
 import styles from './index.module.scss';
+
 import Pie from 'components/Pie'
 
+
 function App() {
+  const [percentage, setPercentage] = useState(100)
   return (
-    <div className={styles.App}>
-      <Pie percentage={99} />
+    <div
+    className={styles.container}
+    >
+      <div
+        className={styles.pieContainer}
+        >
+        <Pie percentage={percentage} />
+      </div>
+      <input
+        className={styles.percentageInput}
+        onChange={({target}) => setPercentage(parseInt(target.value))}
+        type="number"
+        value={percentage}
+      />
     </div>
   );
 }
