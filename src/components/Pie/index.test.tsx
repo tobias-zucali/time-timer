f69ca93efdb191f2ff834688cf1b7b45ab55e72e
Tobias Zucali <tobias@zucali.at>
2022-12-04 16:51:34 +0100

    remove unused tests

--- src/components/Pie/index.test.tsx
+++
@@ -1,8 +0,0 @@
-import { render } from '@testing-library/react';
-import Pie from '.';
-
-test('renders learn react link', () => {
-  render(<Pie percentage={1} />);
-  // const linkElement = screen.getByText(/learn react/i);
-  // expect(linkElement).toBeInTheDocument();
-});
